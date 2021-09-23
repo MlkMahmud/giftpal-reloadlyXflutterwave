ae14136ae0b4e025932d4788b8ddc8d26bba0927
Malik Mahmud <almalikmahmud@gmail.com>
2021-09-23 19:19:45 +0100

    add prop-types

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import Text from '../components/Text';
 
 export default function Index() {
   return (
-    <p>This is a sample page</p>
+    <Text>This is a sample page</Text>
   );
 }
